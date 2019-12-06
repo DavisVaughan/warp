515c6a3eb8e96b7940db3744f5eaeda1da878d26
DavisVaughan <davis@rstudio.com>
2019-12-06 14:47:50 -0500

    Test `locate_changes()`

---
+++ tests/testthat/test-locate-changes.R
@@ -0,0 +1,27 @@
+test_that("can locate changes in an integer vector", {
+  expect_equal(locate_changes(c(1L, 1L, 2L)), c(2, 3))
+})
+
+test_that("can locate changes in a double vector", {
+  expect_equal(locate_changes(c(1, 1, 2)), c(2, 3))
+})
+
+test_that("`NA_integer_` values look equivalent", {
+  expect_equal(locate_changes(c(1, NA_integer_, NA_integer_)), c(1, 3))
+})
+
+test_that("`NA_real_` values look equivalent", {
+  expect_equal(locate_changes(c(1, NA_real_, NA_real_)), c(1, 3))
+})
+
+test_that("`NaN` values look equivalent", {
+  expect_equal(locate_changes(c(1, NaN, NaN)), c(1, 3))
+})
+
+test_that("`NaN` and `NA_real_` values look different", {
+  expect_equal(locate_changes(c(1, NaN, NA_real_)), c(1, 2, 3))
+})
+
+test_that("can only use double / integer", {
+  expect_error(locate_changes("x"), "must be an integer or double vector")
+})
