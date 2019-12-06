96b1c84110147431b2c526c55770e18d24796f09
DavisVaughan <davis@rstudio.com>
2019-12-06 14:56:08 -0500

    Test `locate_boundaries()`

---
+++ tests/testthat/test-locate-boundaries.R
@@ -0,0 +1,21 @@
+test_that("can locate boundaries", {
+  expect <- data.frame(start = 1:2, stop = 1:2)
+  expect_equal(locate_boundaries(c(1, 2)), expect)
+
+  expect <- data.frame(start = c(1, 2), stop = c(1, 3))
+  expect_equal(locate_boundaries(c(1, 2, 2)), expect)
+})
+
+test_that("the first value is always included as the first `start`", {
+  expect <- data.frame(start = c(1, 3), stop = c(2, 4))
+  expect_equal(locate_boundaries(c(1, 1, 2, 2)), expect)
+})
+
+test_that("duplicate non-contiguous values are allowed, and show up in different groups", {
+  expect <- data.frame(start = 1:3, stop = 1:3)
+  expect_equal(locate_boundaries(c(1, 2, 1)), expect)
+})
+
+test_that("can only use an integer or double vector", {
+  expect_error(locate_boundaries("x"), "must be an integer or double vector")
+})
