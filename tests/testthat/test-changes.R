4d8e88499592476bd0e6b29b36426035f9f0c8bd
DavisVaughan <davis@rstudio.com>
2019-12-19 16:30:27 -0500

    Test `warp_change()` and remove `locate_changes()`

---
+++ tests/testthat/test-changes.R
@@ -0,0 +1,29 @@
+test_that("can locate changes", {
+  x <- new_date(c(1, 1, 2))
+  expect_equal(warp_change(x, "day"), c(2, 3))
+})
+
+test_that("the last value's position is always included", {
+  x <- new_date(c(1, 1, 2, 2))
+  result <- warp_change(x, "day")
+
+  expect_equal(result[length(result)], length(x))
+})
+
+test_that("duplicate non-contiguous values are allowed", {
+  x <- new_date(c(1, 1, 2, 1))
+  expect_equal(warp_change(x, "day"), c(2, 3, 4))
+})
+
+test_that("`NA` / `NaN` / `Inf` dates look equivalent", {
+  x <- new_date(c(NA_real_, Inf, NaN))
+  expect_equal(warp_change(x), 3)
+})
+
+test_that("size 0 input works", {
+  expect_equal(warp_change(new_date()), numeric())
+})
+
+test_that("size 1 input works", {
+  expect_equal(warp_change(new_date(0)), 1)
+})
